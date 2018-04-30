8dd7d04eeb7d31e64746adf08c7c51d8bc0ce016
Fenil Gandhi <fanilgandhi@gmail.com>
2018-04-30 20:56:43 +0530

    Solved exercism-isbn-verifier

---
+++ exercism/6.isbn-verifier/isbn_verifier.py
@@ -0,0 +1,24 @@
+def verify(isbn: str) -> bool:
+	isbn = isbn.replace('-', '')
+	# Reject invalid length isbn, return False
+	if len(isbn) != 10:
+		return False
+
+	# If check digit is not correct, return False
+	if isbn[9].isdigit():
+		sum += int(isbn[9])
+	elif isbn[9] == "X":
+		sum += 10
+	else:
+		return False
+
+	sum = 0
+	for index in range(9):
+		if not isbn[index].isdigit():
+			return False
+		sum += int(isbn[index]) * (10 - index)
+
+	sum = sum % 11
+	if sum == 0:
+		return True
+	return False
