9aaebac567709b3f01cefec656b0456e77e3f085
Fenil Gandhi <fanilgandhi@gmail.com>
2018-05-06 09:44:53 +0530

    Patched exercism-6.isbn_verifier

--- exercism/6.isbn-verifier/isbn_verifier.py
+++ exercism/6.isbn-verifier/isbn_verifier.py
@@ -5,6 +5,7 @@ def verify(isbn: str) -> bool:
 		return False
 
 	# If check digit is not correct, return False
+	sum = 0
 	if isbn[9].isdigit():
 		sum += int(isbn[9])
 	elif isbn[9] == "X":
@@ -12,7 +13,6 @@ def verify(isbn: str) -> bool:
 	else:
 		return False
 
-	sum = 0
 	for index in range(9):
 		if not isbn[index].isdigit():
 			return False
